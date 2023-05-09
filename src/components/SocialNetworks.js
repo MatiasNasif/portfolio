ddf5f60ed4f44d40076a17815b7cd8bd68b7f878
MatiasNasif <matiasezequielnasif@gmail.com>
2023-05-09 19:32:58 -0300

    refactor: change cv

--- src/components/SocialNetworks.js
+++ src/components/SocialNetworks.js
@@ -6,7 +6,7 @@ const SocialNetworks = () => {
   return (
     <div className="social-container" id="projects">
         
-        <a href="https://drive.google.com/file/d/1rEnKxrybkttIqHKfmDSfXhrsGnQemz3w/view?usp=share_link" target="_blank" rel="noopener noreferrer">Descargar CV
+        <a href="https://drive.google.com/file/d/1P0Mt8114Mrk7oXYlN8ZugLawiZ9TfT3m/view?usp=share_link" target="_blank" rel="noopener noreferrer">Descargar CV
         <img src={iconDownload} alt="icon download" style={{width: "25px", background:"white", borderRadius:"50%", marginLeft: "0.5rem", padding:"3px"}} />
         </a>
 
